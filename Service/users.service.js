fbc364ebc63e83c2616d893c7bbd464b80090f23
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-09 18:38:44 +0530

    users.service

--- Service/users.service.js
+++ Service/users.service.js
@@ -1,3 +1,20 @@
 const mongo = require("../schema/mongodb");
 
 const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
+const { registerSchema, loginSchema } = require("../schema/schema");
+
+const service = {
+  // register data service
+  async register(req, res) {
+    try {
+      //validation using joi schema(value,error)
+      const { value, error } = await registerSchema.validate(req.body);
+
+      if (error)
+        return res.status(400).send({ Error: error.details[0].message });
+    } catch (err) {}
+  },
+};
