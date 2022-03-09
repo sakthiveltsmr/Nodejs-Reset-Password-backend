edc6c1a73cc2bcc6fc3a2d995b71ae37cf5257f0
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-09 18:54:52 +0530

    user.service

--- Service/users.service.js
+++ Service/users.service.js
@@ -21,6 +21,66 @@ const service = {
         email: req.body.email,
       });
       console.log(emailExist);
-    } catch (err) {}
+      if (emailExist)
+        return res
+          .status(400)
+          .send({ alert: "user already Exist,try with new Email id" });
+      // gen salt using bcrypt
+      const salt = await bcrypt.genSalt(10);
+      console.log("random string", salt);
+
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+      console.log("encrpted password", req.body.password);
+
+      // post the user data to db
+      const postData = await mongo.register.insertOne(req.body);
+      console.log("user register successfully");
+    } catch (err) {
+      console.log("err in registration", err);
+    }
+  },
+
+  // login user service (JWT)
+  async login(req, res) {
+    try {
+      //joi loginschema validation
+      const { value, error } = await loginSchema.validate(req.body);
+
+      if (error)
+        return res.status(400).send({ Error: error.details[0].message });
+
+      //check first email exist;
+      const emailExist = await mongo.register.findOne({
+        email: req.body.email,
+      });
+      //not exist
+      if (!emailExist)
+        return res.status(404).send({ Alert: "User not found,Please Sign up" });
+
+      //check password valid or not bcrypt.campare
+      const passValid = await bcrypt.compare(
+        req.body.password,
+        emailExist.password
+      ); //compare with orignal pass and typed password;
+
+      if (!passValid)
+        return res.status(400).send({ Alert: "Enter the correctPassword" });
+
+      //gen Token using jwt
+      const token = jwt.sign(
+        {
+          userId: emailExist._id,
+        },
+        process.env.JWT_SECRET_KEY,
+        { expiresIn: "8h" }
+      );
+      console.log(token);
+
+      res.status(200).send({ Alert: "Login successfully", token: token });
+    } catch (err) {
+      console.log("error in login", err);
+    }
   },
 };
+
+module.exports = service;
