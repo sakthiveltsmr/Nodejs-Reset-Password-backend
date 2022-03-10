1c9744a0fdcb2297fb2fd8ba2b837e7ce1b72c70
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-10 20:52:25 +0530

    app

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ const app = express();
     app.use(cors());
     app.use(express.json());
 
-    // app.use((req, res, next) => {
-    //   console.log("user middle ware called");
-    //   res.send("server running");
-    //   next();
-    // });
+    app.use((req, res, next) => {
+      console.log("user middle ware called");
+      // res.send("server running");
+      next();
+    });
 
     app.use("/users", userRoute);
 
